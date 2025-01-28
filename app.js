f3370c08ddae311f7d7c90823f2df3874e500d7f
Anto Lamanna <88988992+anto-lmna@users.noreply.github.com>
2025-01-28 18:11:34 -0300

    agrega lógica para agregar amigos y sortearlos

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listadoAmigos = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    
+    if (!nombreAmigo){
+        alert('Por favor, inserte un nombre');
+    }
+    else {
+        listadoAmigos.push(nombreAmigo);
+        listarAmigos();
+        document.getElementById('amigo').value = '';
+        
+    }
+}
+
+function listarAmigos() {
+    let elementoLista = document.getElementById('listaAmigos');
+    elementoLista.innerHTML = '';
+    let amigosLista = '';
+
+    for (let amigo of listadoAmigos) {
+        amigosLista += `<li>${amigo}</li>`;
+    }
+
+    elementoLista.innerHTML = amigosLista;
+}
+
+function generarNumero(){
+    return Math.floor(Math.random() * listadoAmigos.length);
+}
+
+
+function sortearAmigo(){
+    let indiceLista = generarNumero();
+    let elementoLista = document.getElementById('resultado');
+    
+
+    if(listadoAmigos.length > 1){
+        elementoLista.innerHTML = `<li>Tu amigo secreto es ${listadoAmigos[indiceLista]}</li>`
+    }
+    else{
+        alert("Debe haber al menos dos amigos para realizar el sorteo")
+    }
+}
+
+
+
+
+
